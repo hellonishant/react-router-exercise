53eea84e7d47f01736dba5d5b795d76d2e06fef2
Nishant <menishant77@gmail.com>
2020-03-11 03:13:41 +0530

    Route Parmas done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import { Switch, Link, NavLink, Route } from "react-router-dom";
+import Food from "./Food";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// Route Params are given using the /url/:param form and the route props are needed to
+// be passed using through function because that's how render prop works in Route Component
+// while the component props only need the name of the component it needs to render.
+class App extends Component {
+	render() {
+		return (
+			<div className="App">
+				<Route
+					exact
+					path="/food/:name"
+					render={routerProps => <Food {...routerProps} />}
+				/>
+			</div>
+		);
+	}
 }
 
 export default App;
